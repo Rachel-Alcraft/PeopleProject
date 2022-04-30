0d21ca7d142c263b6311fd538f7f2538c35b627c
Rachel-Alcraft <r.alcraft@ucl.ac.uk>
2022-04-30 09:39:42 +0100

    Bug fix, deadlock was renamed finished

--- lib/helper.py
+++ lib/helper.py
@@ -11,7 +11,7 @@ def createPeopleList(people_df):
         prsn = lib.Person.Person(nm,prefs,tm)
         people.append(prsn)
     return people
-
+##########################################################################################
 def createProjectList(projects_df):
     projects = {}
     for i in range(len(projects_df.index)):        
@@ -23,8 +23,8 @@ def createProjectList(projects_df):
         pjct = lib.Project.Project(id,nm,imp,p_d,exp)
         projects[int(id)]=pjct
     return projects
-
-def runAllocator(num_projects,people,projects,max_importance = 0):
+##########################################################################################
+def runAllocator(num_projects,people,projects,max_importance = 0,max_fraction=1):
     finished = False
     while not finished:
         finished = True #only set it to false if any change is made
@@ -34,7 +34,7 @@ def runAllocator(num_projects,people,projects,max_importance = 0):
                     if len(peos.preferences) > i:
                         next_preferred_project = projects[int(peos.preferences[i])]
                         if next_preferred_project.importance >= max_importance and not next_preferred_project.isAllocated():
-                            alloc = lib.Allocation.Allocation(next_preferred_project,peos)
+                            alloc = lib.Allocation.Allocation(next_preferred_project,peos,max_fraction)
                             next_preferred_project.addAllocation(alloc)
-                            deadlock = finished
-        
+                            finished = False
+##########################################################################################
